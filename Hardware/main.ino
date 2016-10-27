a6ba1d6ed6594663579e281044488e0e333a9dc6
Ha Hyun soo <h2s1880@gmail.com>
2016-10-27 20:47:16 +0900

    Update Busking Cloud Hardware program
    大家 加油
    여러분 파이팅입니다!

--- Hardware/main.ino
+++ Hardware/main.ino
@@ -1,9 +1,82 @@
+/*
+*   Title : Busking Cloud
+*   Provider : Kang mi jung, Liu hui yu
+*   Copyright(C)2016 MIRO robot science group
+*/
+
+#define LED_RED 5;        //red color led
+#define LED_GREEN 6;      //green color led
+#define LED_BLUE 7;       //blue color led
+
+void reset_light_function();     //reset light function
+void red_light_function();       //red light function
+void green_light_function();     //green light function
+void blue_light_function();      //blue light function
+
 void setup(){
 
-
+  Serial.begin(9600);            //Bluetooth Serial Setting
+  pinMode(LED_RED, OUTPUT);      //red led output state
+  pinMode(LED_GREEN, OUTPUT);    //green led output state
+  pinMode(LED_BLUE, OUTPUT);     //blue led output state
+  
 }
 
 void loop(){
 
+  if(Serial.available() > 0){
+    
+    char recv_data = (char)Serial.read();            // Recieve Char type data from Mobile
+  
+    if(recv_data == 'R' || recv_data == 'r'){        // recieve data is Red
+    
+      reset_light_function();
+      red_light_function();
+    }
+    else if(recv_data == 'G' || recv_data == 'g'){   // recieve data is Green
+    
+      reset_light_function();
+      green_light_function();
+    }
+    else if(recv_data == 'B' || recv_data == 'b'){   // recieve data is Blue
+    
+      reset_light_function();
+      blue_light_function();
+    }
+    else{                                            // Exception Handling
+    
+      reset_light_function();
+    }
+    
+  }
 
+}
+
+void reset_light_function(){     //reset light function
+
+  //NOTE : Turn off all led
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_BLUE, LOW);
+}
+
+void red_light_function(){       //red light function
+
+  //NOTE : Turn on Red led
+  //TODO : Insert more code, if you need it
+  digitalWrite(LED_RED, HIGH);
+}
+
+void green_light_function(){     //green light function
+
+  //NOTE : Turn on Green led
+  //TODO : Insert more code, if you need it
+  digitalWrite(LED_GREEN, HGIH);
+}
+
+void blue_light_function(){      //blue light function
+  
+  //NOTE : Turn on Blue led
+  //TODO : Insert more code, if you need it
+  digitalWrite(LED_BLUE, HIGH);
 }
